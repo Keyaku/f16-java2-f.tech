7d3abe6b729c09e19835fef43965371d79403da8
graveshooter <57713936+graveshooter@users.noreply.github.com>
2019-11-15 15:06:52 +0000

    Minor Clientes fixes

--- ftech2/src/com/f16/ftech/buisness/Clientes.java
+++ ftech2/src/com/f16/ftech/buisness/Clientes.java
@@ -13,132 +13,140 @@ import com.f16.ftech.factory.ClientesFtechDaoFactory;
 
 @WebService
 public class Clientes {
-@WebMethod
-		public int InsertCliente(ClientesFtech cliente) {
-			ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-			try {
-				clienteDao.insert(cliente);
-			}catch (ClientesFtechDaoException e) {
-				e.printStackTrace();
-			}
-			return 0;
+	@WebMethod
+	public int InsertCliente(ClientesFtech cliente) {
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clienteDao.insert(cliente);
+		}catch (ClientesFtechDaoException e) {
+			e.printStackTrace();
 		}
-@WebMethod
+		return 0;
+	}
+	
+	@WebMethod
 	public ClientesFtech[] ListarTodosClientes() {
-	ClientesFtech[] clientes = null;
-	ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-	try {
-		clientes = clienteDao.findAll();
-	} catch (ClientesFtechDaoException e) {
-		// TODO: handle exception
-		e.printStackTrace();
+		ClientesFtech[] clientes = null;
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clientes = clienteDao.findAll();
+		} catch (ClientesFtechDaoException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+			return clientes;
 	}
+
+	@WebMethod
+	public int DeleteCliente(ClientesFtechPk clientepk) {
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clienteDao.delete(clientepk);
+		}catch (ClientesFtechDaoException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	@WebMethod
+	public int UpdateCliente(ClientesFtechPk clientepk, ClientesFtech cliente) {
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clienteDao.update(clientepk,cliente);
+		}catch (ClientesFtechDaoException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	@WebMethod
+	public ClientesFtech[] ListarClientesPorID(int id) {
+		ClientesFtech[] clientes = null;
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clientes = clienteDao.findWhereIdClienteEquals(id);
+		} catch (ClientesFtechDaoException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 		return clientes;
-}
-
-@WebMethod
-public int DeleteCliente(ClientesFtechPk clientepk) {
-	ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-	try {
-		clienteDao.delete(clientepk);
-	}catch (ClientesFtechDaoException e) {
-		e.printStackTrace();
 	}
-	return 0;
-}
 
-public int UpdateCliente(ClientesFtechPk clientepk, ClientesFtech cliente) {
-	ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-	try {
-		clienteDao.update(clientepk,cliente);
-	}catch (ClientesFtechDaoException e) {
-		e.printStackTrace();
+	@WebMethod
+	public ClientesFtech[] ListarClientesPorCC(int cc) {
+		ClientesFtech[] clientes = null;
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clientes = clienteDao.findWhereNumeroCcEquals(cc);
+		} catch (ClientesFtechDaoException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return clientes;
 	}
-	return 0;
-}
 
-@WebMethod
-public ClientesFtech[] ListarClientesPorID(int id) {
-ClientesFtech[] clientes = null;
-ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-try {
-	clientes = clienteDao.findWhereIdClienteEquals(id);
-} catch (ClientesFtechDaoException e) {
-	// TODO: handle exception
-	e.printStackTrace();
-}
-	return clientes;
-}
+	@WebMethod
+	public ClientesFtech[] ListarClientesPorNome(String nome) {
+		ClientesFtech[] clientes = null;
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clientes = clienteDao.findWhereNomeEquals(nome);
+		} catch (ClientesFtechDaoException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return clientes;
+	}
 
-public ClientesFtech[] ListarClientesPorCC(int cc) {
-ClientesFtech[] clientes = null;
-ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-try {
-	clientes = clienteDao.findWhereNumeroCcEquals(cc);
-} catch (ClientesFtechDaoException e) {
-	// TODO: handle exception
-	e.printStackTrace();
-}
-	return clientes;
-}
+	@WebMethod
+	public ClientesFtech[] ListarClientesPorMorada(String morada) {
+		ClientesFtech[] clientes = null;
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clientes = clienteDao.findWhereMoradaEquals(morada);
+		} catch (ClientesFtechDaoException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return clientes;
+	}
 
-public ClientesFtech[] ListarClientesPorNome(String nome) {
-ClientesFtech[] clientes = null;
-ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-try {
-	clientes = clienteDao.findWhereNomeEquals(nome);
-} catch (ClientesFtechDaoException e) {
-	// TODO: handle exception
-	e.printStackTrace();
-}
-	return clientes;
-}
+	@WebMethod
+	public ClientesFtech[] ListarClientesPorDataNasc(Date nascimento) {
+		ClientesFtech[] clientes = null;
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clientes = clienteDao.findWhereDataNascimentoEquals(nascimento);
+		} catch (ClientesFtechDaoException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return clientes;
+	}
 
-public ClientesFtech[] ListarClientesPorMorada(String morada) {
-ClientesFtech[] clientes = null;
-ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-try {
-	clientes = clienteDao.findWhereMoradaEquals(morada);
-} catch (ClientesFtechDaoException e) {
-	// TODO: handle exception
-	e.printStackTrace();
-}
-	return clientes;
-}
+	@WebMethod
+	public ClientesFtech[] ListarClientesPorDataInsert(Date insert) {
+		ClientesFtech[] clientes = null;
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clientes = clienteDao.findWhereDataInsercaoEquals(insert);
+		} catch (ClientesFtechDaoException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return clientes;
+	}
 
-public ClientesFtech[] ListarClientesPorDataNasc(Date nascimento) {
-ClientesFtech[] clientes = null;
-ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-try {
-	clientes = clienteDao.findWhereDataNascimentoEquals(nascimento);
-} catch (ClientesFtechDaoException e) {
-	// TODO: handle exception
-	e.printStackTrace();
-}
-	return clientes;
-}
-
-public ClientesFtech[] ListarClientesPorDataInsert(Date insert) {
-ClientesFtech[] clientes = null;
-ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-try {
-	clientes = clienteDao.findWhereDataInsercaoEquals(insert);
-} catch (ClientesFtechDaoException e) {
-	// TODO: handle exception
-	e.printStackTrace();
-}
-	return clientes;
-}
-
-public ClientesFtech[] ListarClientesPorGenero(String genero) {
-ClientesFtech[] clientes = null;
-ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-try {
-	clientes = clienteDao.findWhereGeneroEquals(genero);
-} catch (ClientesFtechDaoException e) {
-	// TODO: handle exception
-	e.printStackTrace();
-}
-	return clientes;
-}
+	@WebMethod
+	public ClientesFtech[] ListarClientesPorGenero(String genero) {
+		ClientesFtech[] clientes = null;
+		ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+		try {
+			clientes = clienteDao.findWhereGeneroEquals(genero);
+		} catch (ClientesFtechDaoException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return clientes;
+	}
 }

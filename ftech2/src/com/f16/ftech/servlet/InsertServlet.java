864bed1f7c4ad424d1f76919124e788440b36e23
graveshooter <57713936+graveshooter@users.noreply.github.com>
2019-11-27 12:19:00 +0000

    Update InsertServlet.java

--- ftech2/src/com/f16/ftech/servlet/InsertServlet.java
+++ ftech2/src/com/f16/ftech/servlet/InsertServlet.java
@@ -6,10 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.f16.ftech.buisness.Clientes;
+import com.f16.ftech.business.Clientes;
 import com.f16.ftech.dto.ClientesFtech;
 
-import java.io.PrintWriter;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
  
 
 	public class InsertServlet extends HttpServlet {
@@ -22,10 +25,27 @@ import java.io.PrintWriter;
 	        String birthdate = request.getParameter("birthdate");
 	        String dateInsert = request.getParameter("dateInsert");
 	        String genero = request.getParameter("genero");
+	        
+	        
 	        //criar objeto
 	        ClientesFtech cliente = new ClientesFtech();
-	        cliente.setDataInsercao(dateInsert);
-	        PrintWriter out = response.getWriter();
+	        Date dataInsercao = null;
+	        Date dataNasc = null;
+	        try {
+				dataInsercao=new SimpleDateFormat("yyyy-MM-dd").parse(dateInsert);
+				dataNasc = new SimpleDateFormat("yyyy-MM-dd").parse(birthdate);
+			} catch (java.text.ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        cliente.setDataInsercao(dataInsercao);
+	        cliente.setDataNascimento(dataNasc);
+	        cliente.setGenero(genero);
+	        cliente.setIdCliente(Integer.parseInt(ID));
+	        cliente.setMorada(morada);
+	        cliente.setNumeroCc(Integer.parseInt(CC_number));
+	        cliente.setNome(name);
+	        response.getWriter();
 	        Clientes novoCliente = new Clientes();
 	        novoCliente.InsertCliente(cliente);
 	        }

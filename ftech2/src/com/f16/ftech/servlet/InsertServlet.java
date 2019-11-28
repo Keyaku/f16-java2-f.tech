193a4c61ddc086369095424ded6144fd54045aac
graveshooter <57713936+graveshooter@users.noreply.github.com>
2019-11-28 14:57:43 +0000

    Added some verification on server

--- ftech2/src/com/f16/ftech/servlet/InsertServlet.java
+++ ftech2/src/com/f16/ftech/servlet/InsertServlet.java
@@ -19,55 +19,55 @@ import java.util.Date;
  
 
 @WebServlet("InsertServlet")
-	public class InsertServlet extends HttpServlet {
-	    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	        // reading the user input
-	        String ID = request.getParameter("ID");
-	        String CCnumber = request.getParameter("CCnumber");
-	        String name = request.getParameter("name");
-	        String morada = request.getParameter("morada");
-	        String birthdate = request.getParameter("birthdate");
-	        String dateInsert = request.getParameter("dateInsert");
-	        String genero = request.getParameter("genero");
-	        
-	        
-	        //criar objeto
-	        int clienteID = Integer.parseInt(ID);
-	        ClientesFtech cliente = new ClientesFtech();
-	        Date dataInsercao = null;
-	        Date dataNasc = null;
-	        try {
-				dataInsercao=new SimpleDateFormat("yyyy-MM-dd").parse(dateInsert);
-				dataNasc = new SimpleDateFormat("yyyy-MM-dd").parse(birthdate);
-			} catch (java.text.ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+public class InsertServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // reading the user input
+        String ID = request.getParameter("ID");
+        String CCnumber = request.getParameter("CCnumber");
+        String name = request.getParameter("name");
+        String morada = request.getParameter("morada");
+        String birthdate = request.getParameter("birthdate");
+        String dateInsert = request.getParameter("dateInsert");
+        String genero = request.getParameter("genero");
+
+        int clienteID = Integer.parseInt(ID);
+
+        //criar objeto
+        ClientesFtech cliente = new ClientesFtech();
+        Date dataInsercao = null;
+        Date dataNasc = null;
+        try {
+			dataInsercao = new SimpleDateFormat("yyyy-MM-dd").parse(dateInsert);
+			dataNasc = new SimpleDateFormat("yyyy-MM-dd").parse(birthdate);
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        cliente.setDataInsercao(dataInsercao);
+        cliente.setDataNascimento(dataNasc);
+        cliente.setGenero(genero);
+        cliente.setIdCliente(clienteID);
+        cliente.setMorada(morada);
+        cliente.setNumeroCc(Integer.parseInt(CCnumber));
+        cliente.setNome(name);
+      
+        Clientes clienteNG = new Clientes();
+    	ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
+    	try {
+			if (clienteDao.findByPrimaryKey(clienteID) != null) {
+				clienteNG.UpdateCliente(clienteID, cliente);
+			} else {
+				clienteNG.InsertCliente(cliente);
 			}
-	        cliente.setDataInsercao(dataInsercao);
-	        cliente.setDataNascimento(dataNasc);
-	        cliente.setGenero(genero);
-	        cliente.setIdCliente(clienteID);
-	        cliente.setMorada(morada);
-	        cliente.setNumeroCc(Integer.parseInt(CCnumber));
-	        cliente.setNome(name);
-	      
-	        Clientes clienteNG = new Clientes();
-	    	ClientesFtechDao clienteDao = ClientesFtechDaoFactory.create();
-	    	try {
-				if (clienteDao.findByPrimaryKey(clienteID) != null) {
-					// do update
-					clienteNG.UpdateCliente(clienteID, cliente);
-				} else {
-					clienteNG.InsertCliente(cliente);
-				}
-			} catch (ClientesFtechDaoException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	        
-	        request.setAttribute("clienteID", ID);
-	        request.getRequestDispatcher("/ClienteDetails.jsp").forward(request, response);	        
-	    }
-	}
+		} catch (ClientesFtechDaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        request.setAttribute("clienteID", ID);
+        request.getRequestDispatcher("/ClienteDetails.jsp").forward(request, response);	 
+
+    }
+}
 
 
